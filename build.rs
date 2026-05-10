5eecb9e6b33ba778150f9ead42d96f28b9ad43df
Yuto Takano <moa17stock@gmail.com>
2026-05-10 17:51:13 +0200

    Rename cxxqt_object.rs to book.rs

--- build.rs
+++ build.rs
@@ -8,6 +8,6 @@ fn main() {
         // - Qt Qml is linked by enabling the qt_qml Cargo feature of cxx-qt-lib.
         // - Qt Qml requires linking Qt Network on macOS
         .qt_module("Network")
-        .files(["src/cxxqt_object.rs"])
+        .files(["src/book.rs"])
         .build();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-pub mod cxxqt_object;
+pub mod book;
 
 use cxx_qt::casting::Upcast;
 use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QQmlEngine, QUrl};
