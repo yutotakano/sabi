dc142055b8b185d898c0c61f0d51e9ffe82bc22f
Yuto Takano <moa17stock@gmail.com>
2026-05-11 19:02:18 +0200

    Implement EpubManifest and EpubManifestEntry parser

--- src/epubmanifest.cpp
+++ src/epubmanifest.cpp
@@ -1,9 +1,49 @@
+#include <iostream>
+
 #include "epubmanifest.h"
 
 EpubManifest::EpubManifest(pugi::xml_node manifestNode)
 {
+    pugi::xpath_node_set itemNodes = manifestNode.select_nodes("item");
+    for (pugi::xpath_node itemNode : itemNodes)
+    {
+        m_entries.push_back(new EpubManifestEntry(itemNode.node()));
+        m_idToIndex[itemNode.node().attribute("id").value()] = m_entries.size() - 1;
+    }
+
+    // Debug output
+    std::cout << "Manifest entries:" << std::endl;
+    for (EpubManifestEntry *entry : m_entries)
+    {
+        std::cout << "  id: " << entry->id() << ", href: " << entry->href() << ", media-type: " << entry->mediaType() << std::endl;
+    }
 }
 
 EpubManifest::~EpubManifest()
+{
+    for (EpubManifestEntry *entry : m_entries)
+    {
+        delete entry;
+    }
+}
+
+EpubManifestEntry *EpubManifest::entryById(const std::string &id)
+{
+    auto it = m_idToIndex.find(id);
+    if (it != m_idToIndex.end())
+    {
+        return m_entries[it->second];
+    }
+    return nullptr;
+}
+
+EpubManifestEntry::EpubManifestEntry(pugi::xml_node itemNode)
+{
+    m_id = itemNode.attribute("id").value();
+    m_href = itemNode.attribute("href").value();
+    m_media_type = itemNode.attribute("media-type").value();
+}
+
+EpubManifestEntry::~EpubManifestEntry()
 {
 }
--- src/epubmanifest.h
+++ src/epubmanifest.h
@@ -1,13 +1,39 @@
 #ifndef EPUBMANIFEST_H
 #define EPUBMANIFEST_H
 
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 #include "pugixml.hpp"
+
+class EpubManifestEntry
+{
+public:
+    EpubManifestEntry(pugi::xml_node itemNode);
+    ~EpubManifestEntry();
+
+    std::string id() const { return m_id; }
+    std::string href() const { return m_href; }
+    std::string mediaType() const { return m_media_type; }
+
+private:
+    std::string m_id;
+    std::string m_href;
+    std::string m_media_type;
+};
 
 class EpubManifest
 {
 public:
     EpubManifest(pugi::xml_node manifestNode);
     ~EpubManifest();
+
+    EpubManifestEntry *entryById(const std::string &id);
+
+private:
+    std::vector<EpubManifestEntry *> m_entries;
+    std::unordered_map<std::string, size_t> m_idToIndex;
 };
 
 #endif // EPUBMANIFEST_H
