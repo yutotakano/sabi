3deb7662e8e47e3d850bd8642771e3d3af44bbd6
Yuto Takano <moa17stock@gmail.com>
2026-05-11 14:16:05 +0200

    Fix declaration order in epub.h

--- src/epub.h
+++ src/epub.h
@@ -4,26 +4,6 @@
 #include <string>
 #include <filesystem>
 
-class Epub
-{
-public:
-    Epub(const std::filesystem::path &path);
-    ~Epub();
-
-    EpubPackage *package();
-    EpubMetadata *metadata();
-    EpubManifest *manifest();
-    EpubSpine *spine();
-    EpubToc *toc();
-
-private:
-    EpubPackage *m_package;
-    EpubMetadata *m_metadata;
-    EpubManifest *m_manifest;
-    EpubSpine *m_spine;
-    EpubToc *m_toc;
-};
-
 class EpubPackage
 {
 public:
@@ -57,6 +37,26 @@ class EpubToc
 public:
     EpubToc();
     ~EpubToc();
+};
+
+class Epub
+{
+public:
+    Epub(const std::filesystem::path &path);
+    ~Epub();
+
+    EpubPackage *package();
+    EpubMetadata *metadata();
+    EpubManifest *manifest();
+    EpubSpine *spine();
+    EpubToc *toc();
+
+private:
+    EpubPackage *m_package;
+    EpubMetadata *m_metadata;
+    EpubManifest *m_manifest;
+    EpubSpine *m_spine;
+    EpubToc *m_toc;
 };
 
 #endif // EPUB_H
