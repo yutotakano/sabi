5c04010abfe8040d589380b58e54041991c148d4
Yuto Takano <moa17stock@gmail.com>
2026-05-12 17:32:18 +0200

    Switch to windows11 Qt style to support dark theme

--- src/main.cpp
+++ src/main.cpp
@@ -32,13 +32,13 @@ int main(int argc, char *argv[])
 
     QApplication app(argc, argv);
 
-    app.setStyle("windowsvista");
+    app.setStyle("windows11");
     QMainWindow widget;
     Ui::MainWindow ui;
     ui.setupUi(&widget);
 
     QWebEngineView view;
-    view.setHtml("<html><body><h1>Hello, World! </h1><p>This is a simple web view.</p></body></html>");
+    view.setHtml("<html><head><style>@media (prefers-color-scheme: dark) { html { color: #ffffff; }}</style></head><body><h1>Hello, World! </h1><p>This is a simple web view.</p></body></html>");
     view.page()->setBackgroundColor(Qt::transparent);
 
     ui.webview->addWidget(&view);
