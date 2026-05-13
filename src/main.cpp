28dae5a0604500d55741c5e9a33e738216b742e3
Yuto Takano <moa17stock@gmail.com>
2026-05-14 00:38:08 +0200

    Replace QWebEngineView with QTextBrowser due to performance issues
    QWebEngineView supports more of the HTML/CSS spec than QTextBrowser,
    (and also the full JavaScript engine, which the latter doesn't
    support). But, I found that having a QWebEngineView widget on the
    window causes Qt to start showing massive amounts of visual
    artifacts on the right and bottom edge of the window when resizing.
    
    This did not change even if the Chromium engine was given the
    --disable-gpu flag (verified via about://gpu), or if the main
    window was given hint attributes to not use the GPU (which it
    shouldn't anyway, QtWidgets isn't hardware accelerated)
    
    The presumed reason for the flickering is that since Chromium has
    to run on a different thread, there is a rendering buffer transfer
    between the main drawing thread and Chromium. This causes at least
    a single frame to be out-of-sync between changing the actual size
    of the window and changing the size of the Chromium render context.
    (Which is known to be slow, especially if OpenGL is involved - even
    if it uses a software renderer).
    
    I've looked around on Qt's support forums and StackOverflow/Reddit
    but it doesn't seem to be widely discussed. But I think this is
    unacceptable and completely detracts from the "native" experience,
    so I'm changing to QTextBrowser. Hopefully any shortcomings in
    the future of the new widget can be addressed/extended without
    much pain.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include <QApplication>
-#include <QWebEngineView>
+#include <QTextBrowser>
 
 #include "ui_mainwindow.h"
 
@@ -37,25 +37,18 @@ int main(int argc, char *argv[])
     Ui::MainWindow ui;
     ui.setupUi(&widget);
 
-    QWebEngineView view;
+    QTextBrowser view;
     view.setHtml("<html><head><style>@media (prefers-color-scheme: dark) { html { color: #ffffff; }}</style></head><body><h1>Hello, World! </h1><p>This is a simple web view.</p></body></html>");
-    view.page()->setBackgroundColor(Qt::transparent);
+    view.setStyleSheet("background: transparent");
+    view.setFrameStyle(QFrame::NoFrame);
 
     ui.webview->addWidget(&view);
 
     // Quit on button press
     ui.pushButton->connect(ui.pushButton, &QPushButton::clicked, &app, &QApplication::quit);
 
-    // Only show main window when the web view is loaded
-    QObject::connect(&view, &QWebEngineView::loadFinished, [&widget](bool ok)
-    {
-        if (ok)
-        {
-            widget.show();
-        }
-    });
-
     Epub epub(std::filesystem::path("test/romeo_and_juliet_pg1513.epub"));
 
+    widget.show();
     return app.exec();
 }
